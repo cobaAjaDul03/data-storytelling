7665d47f02277ab7fa628476b02bdbf4647855fd
cobaAjaDul03 <hanief@MacBook-Pro-muhammad.local>
2023-09-18 22:28:45 +0700

    updated

--- components/isi/kedua.tsx
+++ components/isi/kedua.tsx
@@ -4,6 +4,16 @@ const kedua = () => {
   return (
     <div className="indent-10">
       <p>
+        Banyak orang sering bertanya, bagaimana cara mengukur angka kemiskinan?
+        Untuk mengukur kemiskinan, BPS menggunakan konsep kemampuan memenuhi
+        kebutuhan dasar (basic needs approach). Dengan pendekatan ini,
+        kemiskinan dipandang sebagai ketidakmampuan dari sisi ekonomi untuk
+        memenuhi kebutuhan dasar makanan dan bukan makanan yang diukur dari sisi
+        pengeluaran. Jadi Penduduk Miskin adalah penduduk yang memiliki
+        rata-rata pengeluaran perkapita perbulan dibawah garis kemiskinan. Lalu,
+        apa itu garis kemiskinan?
+      </p>
+      <p>
         Pengukuran kemiskinan yang dilakukan oleh BPS adalah salah satu
         instrumen penting dalam merencanakan dan melaksanakan kebijakan sosial
         dan ekonomi di Indonesia.
